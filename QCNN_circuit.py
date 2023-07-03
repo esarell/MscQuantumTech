aac576655cadcdf3d64d16cbdc0c9b00924751f0
Elizabeth Sarell <sc18ess@leeds.ac.uk>
2023-07-03 22:15:15 +0100

    Key Documents

---
+++ QCNN_circuit.py
@@ -0,0 +1,224 @@
+import pennylane as qml
+
+import Benchmarking
+# This module contains the set of unitary ansatze that will be used to benchmark the performances of Quantum Convolutional Neural Network (QCNN) in QCNN.ipynb module
+
+
+# Unitary Ansatze for Convolutional Layer
+def U_TTN(params, wires):  # 2 params
+    qml.RY(params[0], wires=wires[0])
+    qml.RY(params[1], wires=wires[1])
+    qml.CNOT(wires=[wires[0], wires[1]])
+
+
+def U_5(params, wires):  # 10 params
+    qml.RX(params[0], wires=wires[0])
+    qml.RX(params[1], wires=wires[1])
+    qml.RZ(params[2], wires=wires[0])
+    qml.RZ(params[3], wires=wires[1])
+    qml.CRZ(params[4], wires=[wires[1], wires[0]])
+    qml.CRZ(params[5], wires=[wires[0], wires[1]])
+    qml.RX(params[6], wires=wires[0])
+    qml.RX(params[7], wires=wires[1])
+    qml.RZ(params[8], wires=wires[0])
+    qml.RZ(params[9], wires=wires[1])
+
+
+def U_6(params, wires):  # 10 params
+    qml.RX(params[0], wires=wires[0])
+    qml.RX(params[1], wires=wires[1])
+    qml.RZ(params[2], wires=wires[0])
+    qml.RZ(params[3], wires=wires[1])
+    qml.CRX(params[4], wires=[wires[1], wires[0]])
+    qml.CRX(params[5], wires=[wires[0], wires[1]])
+    qml.RX(params[6], wires=wires[0])
+    qml.RX(params[7], wires=wires[1])
+    qml.RZ(params[8], wires=wires[0])
+    qml.RZ(params[9], wires=wires[1])
+
+
+def U_9(params, wires):  # 2 params
+    qml.Hadamard(wires=wires[0])
+    qml.Hadamard(wires=wires[1])
+    qml.CZ(wires=[wires[0], wires[1]])
+    qml.RX(params[0], wires=wires[0])
+    qml.RX(params[1], wires=wires[1])
+
+
+def U_13(params, wires):  # 6 params
+    qml.RY(params[0], wires=wires[0])
+    qml.RY(params[1], wires=wires[1])
+    qml.CRZ(params[2], wires=[wires[1], wires[0]])
+    qml.RY(params[3], wires=wires[0])
+    qml.RY(params[4], wires=wires[1])
+    qml.CRZ(params[5], wires=[wires[0], wires[1]])
+
+
+def U_14(params, wires):  # 6 params
+    qml.RY(params[0], wires=wires[0])
+    qml.RY(params[1], wires=wires[1])
+    qml.CRX(params[2], wires=[wires[1], wires[0]])
+    qml.RY(params[3], wires=wires[0])
+    qml.RY(params[4], wires=wires[1])
+    qml.CRX(params[5], wires=[wires[0], wires[1]])
+
+
+def U_15(params, wires):  # 4 params
+    qml.RY(params[0], wires=wires[0])
+    qml.RY(params[1], wires=wires[1])
+    qml.CNOT(wires=[wires[1], wires[0]])
+    qml.RY(params[2], wires=wires[0])
+    qml.RY(params[3], wires=wires[1])
+    qml.CNOT(wires=[wires[0], wires[1]])
+
+
+def U_SO4(params, wires):  # 6 params
+    qml.RY(params[0], wires=wires[0])
+    qml.RY(params[1], wires=wires[1])
+    qml.CNOT(wires=[wires[0], wires[1]])
+    qml.RY(params[2], wires=wires[0])
+    qml.RY(params[3], wires=wires[1])
+    qml.CNOT(wires=[wires[0], wires[1]])
+    qml.RY(params[4], wires=wires[0])
+    qml.RY(params[5], wires=wires[1])
+
+
+def U_SU4(params, wires): # 15 params
+    qml.U3(params[0], params[1], params[2], wires=wires[0])
+    qml.U3(params[3], params[4], params[5], wires=wires[1])
+    qml.CNOT(wires=[wires[0], wires[1]])
+    qml.RY(params[6], wires=wires[0])
+    qml.RZ(params[7], wires=wires[1])
+    qml.CNOT(wires=[wires[1], wires[0]])
+    qml.RY(params[8], wires=wires[0])
+    qml.CNOT(wires=[wires[0], wires[1]])
+    qml.U3(params[9], params[10], params[11], wires=wires[0])
+    qml.U3(params[12], params[13], params[14], wires=wires[1])
+
+# Pooling Layer
+
+def Pooling_ansatz1(params, wires): #2 params
+    qml.CRZ(params[0], wires=[wires[0], wires[1]])
+    qml.PauliX(wires=wires[0])
+    qml.CRX(params[1], wires=[wires[0], wires[1]])
+
+def Pooling_ansatz2(wires): #0 params
+    qml.CRZ(wires=[wires[0], wires[1]])
+
+'''def Pooling_ansatz3(*params, wires): #3 params
+    qml.CRot(*params, wires=[wires[0], wires[1]])'''
+
+# Quantum Circuits for Convolutional layers
+def conv_layer1(U, params):
+    U(params, wires=[0, 7])
+    for i in range(0, 8, 2):
+        U(params, wires=[i, i + 1])
+    for i in range(1, 7, 2):
+        U(params, wires=[i, i + 1])
+def conv_layer2(U, params):
+    U(params, wires=[0, 6])
+    U(params, wires=[0, 2])
+    U(params, wires=[4, 6])
+    U(params, wires=[2, 4])
+def conv_layer3(U, params):
+    U(params, wires=[0,4])
+
+# Quantum Circuits for Pooling layers
+def pooling_layer1(V, params):
+    for i in range(0, 8, 2):
+        V(params, wires=[i + 1, i])
+def pooling_layer2(V, params):
+    V(params, wires=[2,0])
+    V(params, wires=[6,4])
+def pooling_layer3(V, params):
+    V(params, wires=[0,4])
+
+
+
+def QCNN_structure(U, params, U_params):
+    param1 = params[0:U_params]
+    param2 = params[U_params: 2 * U_params]
+    param3 = params[2 * U_params: 3 * U_params]
+    param4 = params[3 * U_params: 3 * U_params + 2]
+    param5 = params[3 * U_params + 2: 3 * U_params + 4]
+    param6 = params[3 * U_params + 4: 3 * U_params + 6]
+
+    # Pooling Ansatz1 is used by default
+    conv_layer1(U, param1)
+    pooling_layer1(Pooling_ansatz1, param4)
+    conv_layer2(U, param2)
+    pooling_layer2(Pooling_ansatz1, param5)
+    conv_layer3(U, param3)
+    pooling_layer3(Pooling_ansatz1, param6)
+
+
+def QCNN_structure_without_pooling(U, params, U_params):
+    param1 = params[0:U_params]
+    param2 = params[U_params: 2 * U_params]
+    param3 = params[2 * U_params: 3 * U_params]
+
+    conv_layer1(U, param1)
+    conv_layer2(U, param2)
+    conv_layer3(U, param3)
+
+def QCNN_1D_circuit(U, params, U_params):
+    param1 = params[0: U_params]
+    param2 = params[U_params: 2*U_params]
+    param3 = params[2*U_params: 3*U_params]
+
+    for i in range(0, 8, 2):
+        U(param1, wires=[i, i + 1])
+    for i in range(1, 7, 2):
+        U(param1, wires=[i, i + 1])
+
+    U(param2, wires=[2,3])
+    U(param2, wires=[4,5])
+    U(param3, wires=[3,4])
+
+
+
+dev = qml.device('default.qubit', wires = 8)
+@qml.qnode(dev)
+def QCNN(X, params, U, U_params):
+    
+    '''
+    Pass data through QCNN
+    ARGS: X- Data
+          params- tunable parameters of entire quantum circuits
+          U- Unitary ansatz
+          U_params- tunable parameters of single circuit
+    RETURNS: Measurement of final qubit probablility
+    '''
+    Benchmarking.data_embedding(X)#, embedding_type=embedding_type)
+
+    # Quantum Convolutional Neural Network
+    if U == 'U_TTN':
+        QCNN_structure(U_TTN, params, U_params)
+    elif U == 'U_5':
+        QCNN_structure(U_5, params, U_params)
+    elif U == 'U_6':
+        QCNN_structure(U_6, params, U_params)
+    elif U == 'U_9':
+        QCNN_structure(U_9, params, U_params)
+    elif U == 'U_13':
+        QCNN_structure(U_13, params, U_params)
+    elif U == 'U_14':
+        QCNN_structure(U_14, params, U_params)
+    elif U == 'U_15':
+        QCNN_structure(U_15, params, U_params)
+    elif U == 'U_SO4':
+        QCNN_structure(U_SO4, params, U_params)
+    elif U == 'U_SU4':
+        QCNN_structure(U_SU4, params, U_params)
+    elif U == 'U_SU4_no_pooling':
+        QCNN_structure_without_pooling(U_SU4, params, U_params)
+    elif U == 'U_SU4_1D':
+        QCNN_1D_circuit(U_SU4, params, U_params)
+    elif U == 'U_9_1D':
+        QCNN_1D_circuit(U_9, params, U_params)
+    else:
+        print("Invalid Unitary Ansatz")
+        return False
+
+    result = qml.probs(wires=4)
+    return result
---
+++ QCNN_run.py
@@ -0,0 +1,114 @@
+import Benchmarking
+import sin_generator
+import Quantum_Data
+import numpy as np
+
+"""
+Here are possible combinations of benchmarking user could try.
+Unitaries: ['U_TTN', 'U_5', 'U_6', 'U_9', 'U_13', 'U_14', 'U_15', 'U_SO4', 'U_SU4', 'U_SU4_no_pooling', 'U_SU4_1D', 'U_9_1D']
+U_num_params: [2, 10, 10, 2, 6, 6, 4, 6, 15, 15, 15, 2]
+
+circuit: 'QCNN' 
+cost_fn: 'cross_entropy'
+Note: when using 'mse' as cost_fn binary="True" is recommended, when using 'cross_entropy' as cost_fn must be binary="False".
+"""
+
+#Unitaries = ['U_13', 'U_14', 'U_15', 'U_SO4']
+
+Unitaries=['U_6']#, 'U_9', 'U_13', 'U_14', 'U_15', 'U_SO4', 'U_SU4', 'U_SU4_no_pooling', 'U_SU4_1D', 'U_9_1D']
+#U_num_params = [6, 6, 4, 6]
+
+U_num_params = [10]#, 10, 2, 6, 6, 4, 6, 15, 15, 15, 2]
+#dataset = sin_generator.sin_gen(10,10000)
+
+binary = False
+#cost_fn = 'cross_entropy'
+#freqs = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
+#freqs = [1,3,5,7,9,11]
+#filename='Reg Results'
+#Runs the Benchmarking function
+
+filename='/Quantum_Results/Quantum Results U_6 new new new'
+
+fname =r'C:\Users\ivoll\OneDrive\Documents\qml_sin-3 copy\Quantum_data\qdata_10000_0.1.txt'
+with open(fname) as f:
+    lines = f.readlines()
+    all=''.join(lines)
+    allstates=all.split(')], ')
+    #print(len(allstates))
+    labels=allstates[1]
+    labels=labels[:-1]
+    labels=labels.strip(']')
+    labels=labels.strip('[')
+    labels=labels.split(',')
+    labels=[eval(i) for i in labels]
+    #print(labels)
+    #print(all[len(labels):])
+    #all= all[:len(labels)]
+    #all = all.replace('[array(','')
+    all = all.split('),')
+    #print(all)
+    print(len(all))
+    jj=0
+    all_new=[]
+    for item in all:
+    #    print(item)
+        item = item.replace('[array(','')
+        item = item.replace('rray([','')
+        item=item[2:-1]
+        #print(item)
+        item=item.replace('\n       ','')
+        item=item.replace('  ','')
+        item=item.replace(' ','')
+        item=item.split(',')
+        #print(item)
+        #print(len(item))
+        #print(jj,len(item))
+
+        if jj == 9999:
+            
+            item=';'.join(item)
+            #print(item)
+            item=item.split(')')
+            item=item[0]
+            item=item[:-1]
+            #print(item)
+            item=item.split(';')
+            print('testsdvhfsuyds',len(item))
+        if len(item) == 256:
+            gg=[]
+            for i in item:
+                #try:
+                h=np.complex128(i)
+                gg.append(h)
+                #gg= np.append(gg,h)
+                #except:
+                #print(i)
+                #print(i)
+            
+        #    break
+            jj=jj+1
+        #break
+        gg=np.array(gg,dtype=complex)
+        #print(type(gg))
+        all_new.append(gg)
+print(len(all_new))
+print(type(all_new[0]),labels[0])
+#output=[all_new,labels]
+#print(output)
+print('jj:',jj)
+
+    #for line in lines:
+    #    print(line)
+dataset=[all_new,labels]
+#print(len(dataset))
+
+freqs=[0.1]
+print('data read')
+for p in freqs:
+    #dataset = Quantum_Data.quantum_data(p)
+    #dataset = Quantum_Data.sin_gen(p, 10000)
+    Benchmarking.Benchmarking(dataset, Unitaries, U_num_params, filename, circuit='QCNN', steps = 500, snr=p, binary=binary)
+
+
+    #train pnoise network with gnoise
